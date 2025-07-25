d7fa74f78c2723f528db3452dd99de6e6e93a6bd
belenamiune <belen.amiune@globallogic.com>
2025-07-25 14:38:03 -0300

    Put dynamic data

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -9,6 +9,8 @@ import {
   selectProductosState,
   loadProductos
 } from '@features/productos/store';
+import { loadCategorias } from '@features/categorias/store';
+import { selectCategorias } from '@app/features/categorias/store/categorias.selectors';
 
 @Component({
   selector: 'app-dashboard-home',
@@ -22,10 +24,42 @@ export class DashboardComponent implements OnInit {
   error$: Observable<string | null> = this.store
     .select(selectProductosState)
     .pipe(map(s => s.error));
+  categorias$: Observable<any[]> = this.store.select(selectCategorias);
 
   constructor(private store: Store) {}
 
   ngOnInit(): void {
     this.store.dispatch(loadProductos());
+    this.store.dispatch(loadCategorias()); 
   }
+
+  getCategoriaStyles(nombre: string): { bg: string; text: string; icon: string } {
+  switch (nombre.toLowerCase()) {
+    case 'lubricantes':
+      return {
+        bg: 'bg-blue-100 dark:bg-blue-900',
+        text: 'text-blue-700 dark:text-blue-300',
+        icon: 'pi pi-car'
+      };
+    case 'filtros':
+      return {
+        bg: 'bg-purple-100 dark:bg-purple-900',
+        text: 'text-purple-700 dark:text-purple-300',
+        icon: 'pi pi-filter'
+      };
+    case 'repuestos':
+      return {
+        bg: 'bg-yellow-100 dark:bg-yellow-900',
+        text: 'text-yellow-800 dark:text-yellow-300',
+        icon: 'pi pi-cog'
+      };
+    default:
+      return {
+        bg: 'bg-gray-100 dark:bg-gray-800',
+        text: 'text-gray-700 dark:text-gray-300',
+        icon: 'pi pi-tag'
+      };
+  }
+}
+
 }
